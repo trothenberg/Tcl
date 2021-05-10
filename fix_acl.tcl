be2a7f08d5ff518f5d44482ebfbb77eb1ec01a2c
tim <72965656+trothenberg@users.noreply.github.com>
2021-05-10 08:29:15 +0200

    Updated logic for checking i/fs with ACL applied

--- fix_acl.tcl
+++ fix_acl.tcl
@@ -9,7 +9,49 @@ namespace import ::cisco::lib::*
 set errorInfo ""
 set obj_group_name "local_ips"
 set acl_name "external_acl"
+set int_br [exec "show ip interface brief"]
 ###########
+
+#Functions
+proc get_intf_names {} {
+    # Returns list of every interface on device, regardless \
+    # of whether or not it has an IP address assigned.
+    return [regexp -all -line -inline {^\S+} $int_br]
+}
+
+proc get_intf_ips {} {
+    # Returns list with every local interface IP
+    # **Will not catch secondary IPs**
+    return [regexp -all -inline {\d+\.\d+\.\d+\.\d+} $int_br]
+    
+    # RegExp to match IP
+    # (?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)
+    # Router doesn't allow a bad IP to be configured, so an exact match is not necessary
+}
+
+proc get_intf_conf {int} {
+    # Returns running config for specific interface
+    return [exec "show running-config interface $int"]
+}
+
+proc check_acl {conf} {
+    # Checks if $acl_name is applied to interface
+    # in the inbound direction
+    return [string match "*$acl_name in*" $conf]
+}
+
+proc acl_ints {int_list} {
+    # Creates a list of interfaces if check_acl returns true
+    set result ""
+    foreach int $int_list {
+        if [check_acl [get_intf_conf $int]] {
+            lappend $result $int
+        }
+    }
+    return $result
+}
+
+##########################
 
 # Open CLI connection
 if [catch {cli_open} result] {
@@ -18,74 +60,30 @@ if [catch {cli_open} result] {
     array set cli1 $result
 }
 
-
-proc get_intf_ips {} {
-    # Returns list with every local interface IP
-    set int_brief [exec "show ip interface brief"]
-    return [regexp -all -inline {\d+\.\d+\.\d+\.\d+} $int_brief]
-    # RegExp to match IP
-    # (?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)
-    # Router doesn't allow bad IP to be configured, so not necessary
-}
-
-proc get_intf_info {} {
-    # Returns list with interfaces and ACLs
-    #
-    # Intentionally does not match admin down interfaces because
-    # the router will not return usable ACL information
-    set show_int [exec "show ip interface | include is up,|is down,|Outgoing access]"]
-    return [string map {"not set" not_set} $show_int]
-}
-
-proc intf_list {} {
-    # Returns list with every interface
-    return [regexp -all -inline -line {^\S+} [get_intf_info]]
-}
-
-proc acl_list {} {
-    # Returns list of ACLs (or not_set) on every interface
-    set acl_line [regexp -all -inline -line {Outgoing access.*} [get_intf_info]]
-    #^Adding extra characters to list, not sure why
-    return
-}
-
-proc get_acl_ints {} {    
-    # Returns list of interface names to switch desired ACL is applied
-    set result ""
-    set int [intf_list]
-    set acl [acl_list]
-
-    # Compare intf_list with acl_list; if lengths don't match, throw error
-    if {[llength $int] eq [llength $acl]} {
-        set length [llength $int]
-    } else {
-        # Throw error and terminate script
-    }
-
-    for {set i 0} {i<$length} {incr i} {
-        if {[lindex $acl $i] eq $acl_name} {
-            lappend $result [lindex $acl $i]
-        }
-    }
-    return $result
-}
-
-##########################
-
 # Create object group local_ip with every local interface IP on device
 foreach ip [get_intf_ips] {
     ios_config "object-group network $obj_group_name" "host $ip"
 }
 
+# Finds all interfaces with $acl_name applied inbound
+set active_int [acl_ints [get_intf_names]]
+
 # Remove ACL acl_ext from every interface to which is is currently applied
-foreach int [get_acl_ints] {
-    ios_config "interface $int" "no access-group $acl_name out"
+foreach int $active_int {
+    ios_config "interface $int" "no access-group $acl_name in"
 }
+
+# Resequence ACL to ensure 
+ios_config "ip access-list resequence $acl_name 10 10"
 
 # Create new rule for object group local_ips
-ios_config "ip access-list extended $acl_name" ""
+# Putting new rule at beginning for now, but it might make more sense at the end \
+# because
+ios_config "ip access-list extended $acl_name seq 5 permit ip any object-group $obj_group_name"
 
 # Re-apply ACL acl_ext to every interface in array acl_int
-foreach int [get_acl_ints] {
-    ios_config "interface $int" "access-group $acl_name out"
+foreach int $active_int {
+    ios_config "interface $int" "access-group $acl_name in"
 }
+
+# Clean-up tasks
